309384c8238a53677daa9d2d13d5559c38bcd99c
Rudransh Chandel <160801789+RudranshChandell@users.noreply.github.com>
2026-03-13 10:13:40 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (6.83%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int count=0,ans=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='R') count++;
+            else count--;
+            if(count==0) ans++;
+        }
+        return ans;
+    }
+}
