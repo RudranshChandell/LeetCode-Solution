d45a753ba2d633b1a5f68499c6ea10b5e1ff311b
Rudransh Chandel <160801789+RudranshChandell@users.noreply.github.com>
2026-04-12 19:04:10 +0530

    Time: 1 ms (97.22%) | Memory: 43.7 MB (61.06%) - LeetSync

---
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
@@ -0,0 +1,41 @@
+class Solution {
+    int parent[];
+    public boolean equationsPossible(String[] equations) {
+        this.parent=new int[26];
+        for(int i=0;i<26;i++){
+            parent[i]=i;
+        }   
+
+        for(String equation:equations){
+            int firstNode=equation.charAt(0)-'a';
+            int secondNode=equation.charAt(3)-'a';
+
+            if(equation.charAt(1)=='='){
+                int firstParent=findParent(firstNode);
+                int secondParent=findParent(secondNode);
+
+                parent[firstParent]=secondParent;
+            }
+        }
+
+        for(String equation:equations){
+            int firstNode=equation.charAt(0)-'a';
+            int secondNode=equation.charAt(3)-'a';
+
+            if(equation.charAt(1)=='!'){
+                int firstParent=findParent(firstNode);
+                int secondParent=findParent(secondNode);
+
+                if(firstParent==secondParent) return false;
+            }
+        }
+        return true;
+    }
+
+    public int findParent(int x){
+        if(parent[x]!=x){
+            parent[x]=findParent(parent[x]);
+        }
+        return parent[x];
+    }
+}
