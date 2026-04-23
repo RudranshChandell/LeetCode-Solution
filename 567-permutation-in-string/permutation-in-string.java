c3cbf702cb39cc045b8d454e7e87dd9158faa93b
Rudransh Chandel <160801789+RudranshChandell@users.noreply.github.com>
2026-04-23 10:41:48 +0530

    Time: 6 ms (87.84%) | Memory: 43.6 MB (98.38%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int windowSize=s1.length();
+        int wordLength=s2.length();
+        int distinct=0;
+
+        int[]frequency=new int[26];
+
+        for(int i=0;i<windowSize;i++){
+            frequency[s1.charAt(i)-'a']++;
+
+            if(frequency[s1.charAt(i)-'a']==1){
+                distinct++;
+            }
+        }
+
+        for(int i=0;i<wordLength;i++){
+            int currentIndex=s2.charAt(i)-'a';
+            frequency[currentIndex]--;
+            
+            if(frequency[currentIndex]==0){
+                distinct--;
+            }
+            
+            if(i>=windowSize){
+                int leftIndex=s2.charAt(i-windowSize)-'a';
+
+                frequency[leftIndex]++;
+
+                if(frequency[leftIndex]==1){
+                    distinct++;
+                }
+            }
+
+            if(distinct==0) return true;
+        }
+        return false;
+    }
+}
