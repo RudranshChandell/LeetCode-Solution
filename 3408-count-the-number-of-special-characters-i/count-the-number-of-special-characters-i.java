64ecd9d09df433ce6dc29e92f66a234c2b02f158
Rudransh Chandel <160801789+RudranshChandell@users.noreply.github.com>
2026-05-26 22:32:58 +0530

    Time: 3 ms (31.81%) | Memory: 44.3 MB (10.29%) - LeetSync

---
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        HashSet<Character>set=new HashSet<>();
+        HashSet<Character>seen=new HashSet<>();
+        int count=0;
+        for(char ch:word.toCharArray()){
+            char other=Character.isUpperCase(ch)?Character.toLowerCase(ch):Character.toUpperCase(ch);
+            if(set.contains(other) && !set.contains(ch)){
+                count++;
+            }
+
+            set.add(ch);
+        }
+        return count;
+    }
+}
