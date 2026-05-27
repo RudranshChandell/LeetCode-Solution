5b533919358edbda8875b6829a35699f830443a9
Rudransh Chandel <160801789+RudranshChandell@users.noreply.github.com>
2026-05-27 21:33:02 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (41.10%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,31 +1,27 @@
 class Solution {
-    int rows;
-    int cols;
+    int m;
+    int n;
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        this.rows=image.length;
-        this.cols=image[0].length;
-
+        this.m=image.length;
+        this.n=image[0].length;
         int startingColor=image[sr][sc];
-
-        if(startingColor==color) return image;
-
+        if(startingColor==color)return image;
         dfs(image,sr,sc,startingColor,color);
 
         return image;
     }
-
-    public void dfs(int[][] image, int sr, int sc, int startingColor,int color){
+    public void dfs(int[][]image,int sr,int sc,int startingColor,int color){
         image[sr][sc]=color;
 
-        int[]ds={1,0,-1,0,1};
+        int ds[]={1,0,-1,0,1};
 
         for(int i=0;i<4;i++){
-            int nrow=sr+ds[i];
-            int ncol=sc+ds[i+1];
+            int row=sr+ds[i];
+            int col=sc+ds[i+1];
 
-            if(nrow>=0 && nrow<rows && ncol>=0 && ncol<cols && image[nrow][ncol]==startingColor){
-                dfs(image,nrow,ncol,startingColor,color);
-            } 
+            if(row>=0 && row<m && col>=0 && col<n && image[row][col]==startingColor){
+                dfs(image,row,col,startingColor,color);
+            }
         }
     }
 }
