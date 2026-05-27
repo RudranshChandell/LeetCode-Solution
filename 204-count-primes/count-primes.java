735f3f390372f73da657e7bddcd945e5dfd53260
Rudransh Chandel <160801789+RudranshChandell@users.noreply.github.com>
2026-05-27 10:24:44 +0530

    Time: 106 ms (31.12%) | Memory: 51.1 MB (51.65%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n<2) return 0;
+        boolean []arr=new boolean[n];
+        Arrays.fill(arr,true);
+
+        arr[0]=false;
+        arr[1]=false;
+        int count=0;
+
+        for(int i=2;i<n;i++){
+            if(arr[i]){
+                count++;
+                if((long)i*i<n){
+                    for(int j=i*i;j<n;j+=i){
+                        arr[j]=false;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
