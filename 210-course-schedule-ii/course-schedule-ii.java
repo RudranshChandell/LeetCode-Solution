5efb719d81d3c971e309089b46b2e034ed3eb617
Rudransh Chandel <160801789+RudranshChandell@users.noreply.github.com>
2026-06-13 17:09:03 +0530

    Time: 5 ms (84.09%) | Memory: 46.8 MB (67.72%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,26 +1,14 @@
 class Solution {
-    static {
-        Runtime.getRuntime().gc();
-        Runtime.getRuntime().addShutdownHook(new Thread(()->{
-            try(FileWriter f = new FileWriter("display_runtime.txt")){
-                f.write("0");
-            }catch(Exception e){
-
-            }
-        }));
-}
-
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-
         int[]inorder=new int[numCourses];
-        List<List<Integer>>adj=new ArrayList<>();
-        
+        List<List<Integer>>list=new ArrayList<>();
+
         for(int i=0;i<numCourses;i++){
-            adj.add(new ArrayList<>());
+            list.add(new ArrayList<>());
         }
 
-        for(int arr[]:prerequisites){
-            adj.get(arr[1]).add(arr[0]);
+        for(int []arr:prerequisites){
+            list.get(arr[1]).add(arr[0]);
             inorder[arr[0]]++;
         }
 
@@ -30,20 +18,22 @@ class Solution {
                 q.add(i);
             }
         }
+        int j=0,count=0;
         int[]ans=new int[numCourses];
-        int count=0,j=0;
+
         while(!q.isEmpty()){
             int num=q.poll();
             ans[j++]=num;
             count++;
 
-            for(int i:adj.get(num)){
+            for(int i:list.get(num)){
                 inorder[i]--;
                 if(inorder[i]==0){
                     q.add(i);
                 }
             }
         }
-        return (count==numCourses)?ans:new int[0];   
+
+        return count==numCourses?ans:new int[0];
     }
 }
