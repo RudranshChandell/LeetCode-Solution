d79dfa1f276258fe76dbcd273e4c0c8f69221d9d
Rudransh Chandel <160801789+RudranshChandell@users.noreply.github.com>
2026-06-24 05:27:11 +0530

    Time: 7 ms (100.00%) | Memory: 48 MB (12.98%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -9,12 +9,6 @@ class NumArray {
         }
     }
 
-    public void prefix(int[]nums){
-        for(int i=1;i<nums.length;i++){
-            nums[i]+=nums[i-1];
-        }
-    }
-    
     public int sumRange(int left, int right) {
         if(left==0) return prefix[right];
         return prefix[right]-prefix[left-1];
