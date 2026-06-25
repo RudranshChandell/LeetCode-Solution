ab7d8dcedbbbedc1e24b23ad520a281f780098f7
Rudransh Chandel <160801789+RudranshChandell@users.noreply.github.com>
2026-06-25 23:22:10 +0530

    Time: 3 ms (87.63%) | Memory: 51.2 MB (95.28%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,49 +1,32 @@
 class Solution {
-    char[][]grid;
-    int rows;
-    int cols;
-
-    static {
-        Runtime.getRuntime().gc();
-        Runtime.getRuntime().addShutdownHook(new Thread(()->{
-            try(FileWriter f = new FileWriter("display_runtime.txt")){
-                f.write("0");
-            }catch(Exception e){
-
-            }
-        }));
-    }
-
+    int m,n;
     public int numIslands(char[][] grid) {
-        this.grid=grid;
-        this.cols=grid[0].length;
-        this.rows=grid.length;
-        int count=0;
+        int count=0;    
+        m=grid.length;
+        n=grid[0].length;
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    dfs(i,j);
                     count++;
+                    dfs(grid,i,j);
                 }
             }
         }
-
         return count;
     }
 
-    public void dfs(int row,int col){
-        grid[row][col]='0';
-        
+    public void dfs(char[][]grid,int i,int j){
+        if(i<0 ||i>=m || j<0 || j>=n || grid[i][j]=='0'){
+            return;
+        }
+
+        grid[i][j]='0';
+
         int[]ds={1,0,-1,0,1};
 
-        for(int i=0;i<4;i++){
-            int nrow=row+ds[i];
-            int ncol=col+ds[i+1];
-
-            if(nrow>=0 && nrow<rows && ncol>=0 && ncol<cols && grid[nrow][ncol]=='1'){
-                dfs(nrow,ncol);
-            }
+        for(int k=0;k<4;k++){
+            dfs(grid,i+ds[k],j+ds[k+1]);
         }
     }
 }
