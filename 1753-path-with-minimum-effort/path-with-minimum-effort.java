e40dbb267d8e6c9933db7cadc7eb12da6ba0621d
Rudransh Chandel <160801789+RudranshChandell@users.noreply.github.com>
2026-06-26 00:39:43 +0530

    Time: 49 ms (41.25%) | Memory: 48.1 MB (13.72%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,44 +1,42 @@
 class Solution {
     public int minimumEffortPath(int[][] heights) {
-        int row=heights.length;
-        int col=heights[0].length;
-        PriorityQueue<int[]>q=new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
+        int m=heights.length;
+        int n=heights[0].length;
+        PriorityQueue<int[]>pq=new PriorityQueue<>(Comparator.comparingInt((a)->a[0]));
+        pq.add(new int[]{0,0,0});
 
-        int[][]dist=new int[row][col];
-        for(int[]x: dist){
-            Arrays.fill(x,Integer.MAX_VALUE);
+        int[][]dst=new int[m][n];
+        for(int arr[]:dst){
+            Arrays.fill(arr,Integer.MAX_VALUE);
         }
+        dst[0][0]=0;
 
-        dist[0][0]=0;
-        q.add(new int[]{0,0,0});
-
-        while(!q.isEmpty()) {
-            int []arr=q.poll();
-            int dis=arr[0];
+        while(!pq.isEmpty()){
+            int arr[]=pq.poll();
+            int dist=arr[0];
             int r=arr[1];
             int c=arr[2];
 
-            int []ds={0,0,1,-1};
-            int []dr={1,-1,0,0};
-            
-            if(r==row-1 && c==col-1){
-                return dis;
+            int[]ds={1,0,-1,0,1};
+
+            if(r==m-1 && c==n-1){
+                return dist;
             }
-            
+
             for(int i=0;i<4;i++){
-                int newr=r+ds[i];
-                int newc=c+dr[i];
-            
-                if(newr>=0 && newr<row && newc>=0 && newc<col ){
-                    int newVal=Math.max(Math.abs(heights[r][c]-heights[newr][newc]),dis);
-                    
-                    if(newVal<dist[newr][newc]){
-                        dist[newr][newc]=newVal;
-                        q.add(new int[]{newVal,newr,newc});
+                int nr=r+ds[i];
+                int nc=c+ds[i+1];
+
+                if(nr>=0 && nr<m && nc>=0 && nc<n){
+                    int newValue=Math.max(Math.abs(heights[r][c]-heights[nr][nc]),dist);
+
+                    if(newValue<dst[nr][nc]){
+                        dst[nr][nc]=newValue;
+                        pq.add(new int[]{newValue,nr,nc});
                     }
                 }
             }
-        }   
+        }
         return 0;
     }
 }
