cd46140db8904a24df7d9f883fe86781fabca061
Rudransh Chandel <160801789+RudranshChandell@users.noreply.github.com>
2026-08-05 07:05:08 +0530

    Time: 2 ms (98.29%) | Memory: 43.2 MB (58.27%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()==1) return false;
+        if(s.length()==1)return false;
         Stack<Character>stack=new Stack<>();
 
         for(char ch:s.toCharArray()){
-            if(ch=='{' ||ch=='(' || ch=='['){
-                stack.push(ch);
-                continue;
-            }else{
-                if(stack.isEmpty()) return false;
-                char check=stack.pop();
-                if((ch=='}'&& check!='{')||(ch==']'&& check!='[')||(ch==')'&& check!='(')){
-                    return false;
-                }
+            if(ch==')' || ch=='}' || ch==']'){
+                if(stack.isEmpty() || ch!=stack.pop()) return false;
+            }
+            else{
+                stack.push(ch=='('?')':ch=='['?']':'}');
             }
         }
-        if(!stack.isEmpty()) return false;
-        return true;
+        return stack.isEmpty();
+
+
     }
 }
